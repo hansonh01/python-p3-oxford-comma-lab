565bb4e325b215502806bfd3d8a320516982511c
Hanson <hanson.hua903@gmail.com>
2023-11-03 21:33:27 -0500

    Complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) <= 1:
+        return "".join(items)
+    elif len(items) == 2:
+        return " and ".join(items)
+    else:
+        comma_separated = ', '.join(items[:-1])
+        return f"{comma_separated}, and {items[-1]}"
